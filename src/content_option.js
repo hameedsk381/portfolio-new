6a87a0811ba81dad8f28ae947c12bb22998fdef7
hameed Shaik <hameedsk381@gmail.com>
2024-04-04 19:18:48 +0530

    new portfolio

--- src/content_option.js
+++ src/content_option.js
@@ -1,47 +1,43 @@
-const logotext = "JOHN";
+const logotext = "HAMEEDULLAH SHAIK";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Hameedullah Shaik",
+    description: "I’m Hameed _ Full stack devloper,currently working in Servelots,Tumkuru",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Hameed",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I love exploring new tech",
+        second: "I code cool apps",
+        third: "I develop apps using AI",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+description: "Experienced MERN stack developer with one year of hands-on experience in building full-stack applications using MongoDB, Express, React, and Node.js.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "MERN Stack Developer",
+    aboutme: "I am a MERN stack developer who enjoys building modern web apps and exploring the latest JavaScript libraries for both backend and frontend development. I have experience working on decentralized web apps, web scraping, and UI development.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "React developer",
+        where: "Purecode software",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+        jobtitle: "Servelots",
+        where: "Full stack engineer",
+        date: "2024",
+    }
 ];
 
-const skills = [{
-        name: "Python",
+const skills = [
+    {
+        name: "HTML",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "CSS",
         value: 85,
     },
     {
@@ -53,103 +49,86 @@ const skills = [{
         value: 60,
     },
     {
-        name: "Jquery",
+        name: "Node Js",
         value: 85,
     },
+   
+    {
+        name: "Docker",
+        value: 75,
+    },
+    {
+        name: "MongoDB",
+        value: 75,
+    },
+    {
+        name: "Figma",
+        value: 75,
+    },
+    {
+        name: "Github",
+        value: 80,
+    },
+    {
+        name: "Jira",
+        value: 70,
+    },
+    
 ];
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "UI & UX Design services include creating intuitive and visually appealing user interfaces and experiences for web and mobile applications.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Web Apps",
+        description: "Web Apps services involve developing interactive and responsive web applications tailored to meet specific business needs and user requirements.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "NodeJS",
+        description: "NodeJS services encompass building scalable and efficient server-side applications using JavaScript and Node.js runtime environment.",
+    },
+    {
+        title: "Web Scrapping",
+        description: "Web Scrapping services involve extracting and analyzing data from websites to gather valuable insights and information for business purposes.",
+    },
+    {
+        title: "IPFS",
+        description: "IPFS services focus on implementing the InterPlanetary File System (IPFS) for decentralized and peer-to-peer file storage and sharing solutions.",
     },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "https://bafkreic4hzyph5ebvu2hsmekterw3vz4tzj5xb3gfimkjzzvd3ajo4qiai.ipfs.w3s.link",
+    description: "A Web framework for contributing alternative narratives to Web content and to compose renditions based on the user's literacy level or reading-comfort preferences.",
+    link: "https://renarration-ui.vercel.app/",
+  },
+  {
+    img: "https://bafybeif4ic3ubmcsziqyuzfo7gl65ufw2lmidziuhmk7nne2fzklzppgxq.ipfs.w3s.link",
+    description: "My first project where I explored CSS properties in depth, including individual properties like scale and transform.",
+    link: "https://hotstar-d0329.web.app/",
+  },
+  {
+    img: "https://bafybeifdcqixfsiqq3yvswneeacbc7s5sbsscz5ewgapj43etvagx43lvq.ipfs.w3s.link",
+    description: "Its a demo landing website with all bootstrap components designed for a Restaurant",
+    link: "https://coffee-cafe-website.web.app/",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "hameedsk381@gmail.com",
+    YOUR_FONE: "+91 6304752908",
+    description: "Passionate web developer looking forward to work on industrial apps, preferably AI and modern web apps",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+   
 };
 
 const socialprofils = {
-    github: "https://github.com",
+    github: "https://github.com/hameedsk381",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/hameedullah-shaik-249046182",
     twitter: "https://twitter.com",
 };
 export {
